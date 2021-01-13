cb4c0bc1183efadea2ad6d1e538414a053770cd3
Michael Xueyuan Han-Vanbastelaer <hanx@g.harvard.edu>
2021-01-12 20:44:21 -0500

    Add a delete() function in BST. Update test case.

--- catalog/db.py
+++ catalog/db.py
@@ -115,31 +115,83 @@ class BinarySearchTree(object):
         else:
             return self._find_node(curr.left_child, key_or_val)
 
+    def delete(self, key_or_val):
+        """Modify the tree by removing a node if it has the
+        given value (or key if exists). Otherwise, do nothing.
+        This is the public API to delete a node in a tree."""
+        self.root = self._delete_node(self.root, key_or_val)
+
+    def _delete_node(self, curr, key_or_val):
+        """Find a node based on the given value (or key if exists)
+        and delete the node from the tree if it exists. Application
+        should call the public API delete() instead."""
+        if curr is None:
+            return curr
+        curr_val = curr.val
+        if curr.has_key():
+            curr_val = curr.key
+
+        if key_or_val < curr_val:
+            curr.left_child = self._delete_node(curr.left_child, key_or_val)
+        elif key_or_val > curr_val:
+            curr.right_child = self._delete_node(curr.right_child, key_or_val)
+        else:
+            if curr.left_child is None:
+                return curr.right_child
+            elif curr.right_child is None:
+                return curr.left_child
+            candidate = self._min_value_node(curr.right_child)
+
+            curr.val = candidate.val
+            key_or_val = candidate.val
+            if curr.has_key():
+                curr.key = candidate.key
+                key_or_val = candidate.key
+            curr.right_child = self._delete_node(curr.right_child, key_or_val)
+        return curr
+
+    def _max_value_node(self, node):
+        """The node with the maximum value (or key if exists) of a (sub)tree
+        rooted at node. The maximum value is the node itself if it has no
+        right subtree."""
+        if node is None:
+            return None
+        if node.right_child:
+            return self._max_value_node(node.right_child)
+        return node
+
     def _max_value(self, node):
         """The maximum value (or key if exists) of a (sub)tree rooted at node.
         The maximum value is the node itself if it has no right subtree."""
-        if node is None:
+        max_node = self._max_value_node(node)
+        if max_node is None:
             return False
-        if node.right_child:
-            return self._max_value(node.right_child)
+        if max_node.has_key():
+            return max_node.key
         else:
-            if node.key:
-                return node.key
-            else:
-                return node.val
+            return max_node.val
+
+    def _min_value_node(self, node):
+        """The node with the minimum value (or key if exists) of a (sub)tree
+        rooted at node. The minimum value is the node itself if it has no
+        left subtree."""
+        if node is None:
+            return None
+        if node.left_child:
+            return self._min_value_node(node.left_child)
+        else:
+            return node
 
     def _min_value(self, node):
         """The minimum value (or key if exists) of a (sub)tree rooted at node.
         The minimum value is the node itself if it has no left subtree."""
-        if node is None:
+        min_node = self._min_value_node(node)
+        if min_node is None:
             return False
-        if node.left_child:
-            return self._min_value(node.left_child)
+        if min_node.has_key():
+            return min_node.key
         else:
-            if node.key:
-                return node.key
-            else:
-                return node.val
+            return min_node.val
 
     def synthesize(self, node):
         """Synthesize the val (or key if exists) of a node.
@@ -235,4 +287,13 @@ if __name__ == "__main__":
     bst.insert(UntrustedStr("Zack"))
     print(str(bst))
     print(bst.synthesize(bst.root))
+    print(str(bst))
+
+    bst = BinarySearchTree()
+    bst.insert(UntrustedStr("Jake"))
+    bst.insert(UntrustedStr("Blair"))
+    bst.insert(UntrustedStr("Luke"))
+    bst.insert(UntrustedStr("Andre"))
+    bst.insert(UntrustedStr("Zack"))
+    bst.delete("Jake")
     print(str(bst))
