7ab12337426107a57c6175f0a4d836bab8bdea90
Michael Xueyuan Han-Vanbastelaer <hanx@g.harvard.edu>
2021-01-14 10:30:50 -0500

    Minor fix of a test case.

--- catalog/synthesis.py
+++ catalog/synthesis.py
@@ -583,4 +583,4 @@ if __name__ == "__main__":
     synthesizer.reset_constraints()
     synthesizer.eq_constraint(shr32, 0x3E345C, n=2)
     bitvec_val = synthesizer.to_python(synthesizer.value)
-    assert int(bitvec_val) == 16306544, "{val} should be equal to 16306544, but it is not.".format(val=bitvec_val)
+    assert shr32(bitvec_val, n=2) == 0x3E345C
